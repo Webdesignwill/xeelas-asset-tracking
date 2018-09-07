892dc412cc7435d8d67a5464b2adfa6b0b8dae99
Will Falkowski <webdesignwill@Wills-MacBook-Pro.local>
2018-09-07 14:03:11 +0200

    Added map and tweaked css

--- public/static/assets/scripts/main.js
+++ public/static/assets/scripts/main.js
@@ -1,3 +1,13 @@
 "use strict";
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('asset-map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 8
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {}, false);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {}, false)
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById('asset-map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 8
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+}, false)
