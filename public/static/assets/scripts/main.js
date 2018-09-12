475ffd4861d3e9fbafb0685b6b15dcfa258ec26e
Will Falkowski <webdesignwill@Wills-MacBook-Pro.local>
2018-09-12 17:59:10 +0200

    Added additional desktop and mobile features

--- public/static/assets/scripts/main.js
+++ public/static/assets/scripts/main.js
@@ -1,18 +1,77 @@
-"use strict";
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var SidebarListComponent = function SidebarListComponent(componentElement) {
+  _classCallCheck(this, SidebarListComponent);
+
+  var listItem = componentElement.querySelectorAll("[data-component='sidebar-list-component-item']");
+
+  var _loop = function _loop(i) {
+    listItem[i].addEventListener('click', function () {
+      listItem[i].classList.toggle('active');
+    });
+  };
+
+  for (var i = 0; i < listItem.length; i++) {
+    _loop(i);
+  }
+};
+
+var SidebarNav = function () {
+  function SidebarNav(sidebarElement) {
+    _classCallCheck(this, SidebarNav);
+
+    this.bodyEl = document.querySelector('body');
+
+    this.addEvents(document.querySelectorAll('[data-control="toggle-sidebar"]'), 'sidebar-active');
+
+    var listComponent = sidebarElement.querySelectorAll("[data-component='sidebar-list-component']");
+
+    for (var i = 0; i < listComponent.length; i++) {
+      new SidebarListComponent(listComponent[i]);
+    }
+  }
+
+  _createClass(SidebarNav, [{
+    key: 'addEvents',
+    value: function addEvents(toggle, className) {
+      var _this = this;
+
+      for (var i = 0; i < toggle.length; i++) {
+        toggle[i].addEventListener('click', function () {
+          _this.bodyEl.classList.toggle(className);
+        });
+      }
+    }
+  }]);
+
+  return SidebarNav;
+}();
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('asset-map'), {
+  var map = new google.maps.Map(document.getElementById('asset-map'), _defineProperty({
     center: {
       lat: 5.1214,
       lng: -52.0907
     },
     zoom: 8,
-    zoomControl: false,
-    streetViewControl: false
-  });
+    streetViewControl: false,
+    mapTypeControl: false,
+    zoomControl: false
+  }, 'streetViewControl', false));
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+
+  if (document.querySelector('#sidebar')) {
+    new SidebarNav(document.querySelector('#sidebar'));
+  }
+
   var searchCloseToggle = document.querySelector("[data-toggle-icon='search-close']");
   var headerSearch = document.querySelector("[data-component='header-search']");
   if (searchCloseToggle && headerSearch) {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,39 @@
+class SidebarListComponent {
+  constructor(componentElement) {
+    const listItem = componentElement.querySelectorAll("[data-component='sidebar-list-component-item']")
+    for(let i = 0; i<listItem.length; i++) {
+      listItem[i].addEventListener('click', () => {
+        listItem[i].classList.toggle('active')
+      })
+    }
+  }
+}
+
+class SidebarNav {
+  constructor(sidebarElement) {
+
+    this.bodyEl = document.querySelector('body')
+
+    this.addEvents(
+      document.querySelectorAll('[data-control="toggle-sidebar"]'),
+      'sidebar-active'
+    )
+
+    const listComponent = sidebarElement.querySelectorAll("[data-component='sidebar-list-component']")
+
+    for(let i = 0; i<listComponent.length; i++) {
+      new SidebarListComponent(listComponent[i])
+    }
+  }
+
+  addEvents(toggle, className) {
+    for(let i = 0; i< toggle.length; i++) {
+      toggle[i].addEventListener('click', () => {
+        this.bodyEl.classList.toggle(className)
+      })
+    }
+  }
+}
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById('asset-map'), {
@@ -6,12 +42,19 @@ function initMap() {
       lng: -52.0907
     },
     zoom: 8,
+    streetViewControl: false,
+    mapTypeControl: false,
     zoomControl: false,
     streetViewControl: false
   });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  if(document.querySelector('#sidebar')) {
+    new SidebarNav(document.querySelector('#sidebar'))
+  }
+
   const searchCloseToggle = document.querySelector("[data-toggle-icon='search-close']")
   const headerSearch = document.querySelector("[data-component='header-search']")
   if(searchCloseToggle && headerSearch) {
